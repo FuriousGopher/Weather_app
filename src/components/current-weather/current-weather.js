7b353b5154f8c566e198b4fabb44478b2b4f7382
Ivan Kabar <alkazavr94@gmail.com>
2023-06-11 19:49:21 +0200

    Weather_app react plus API

---
+++ src/components/current-weather/current-weather.js
@@ -0,0 +1,15 @@
+import '../current-weather/current-weather.css'
+
+const CurrentWeather = () => {
+    return (
+        <div className='weather'>
+            <div className='top'>
+                <p className='city'>Kyiv</p>
+                <p className='weather-description'>sunny</p>
+            </div>
+            <img src="" alt="weather" className=''/> /////////////// 28min
+        </div>
+    );
+}
+
+export default CurrentWeather;
